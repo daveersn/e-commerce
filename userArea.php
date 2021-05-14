e6ad11f92f0949d7674d3a4a1c8a30f2bc53d458
xDavi01 <xDavi01@gmail.com>
2021-05-14 17:31:08 +0200

    minor changes login

--- userArea.php
+++ userArea.php
@@ -5,21 +5,23 @@ session_start();
 ?>
 
 <body class="bg-gray-100">
-
-<div>
-	<h1 class="text-center p-4 text-3xl font-bold text-white uppercase tracking-wider secondary-btn">
-		<?php echo $_SESSION['displayName'] ?>
-	</h1>
-
-	<div class="mt-6 flex flex-col items-center">
-		<div class="p-6 border shadow-md rounded-md bg-white w-36 h-36 text-center">
-			<i class="ml-2 far fa-address-book text-7xl primary"></i>
-			<h2 class="p-2 uppercase text-xl text-gray-800 font-bold tracking-wider">Ordini</h2>
-		</div>
-	</div>
-</div>
-	
-<?php include 'common/navbar.php' ?>
+    <div>
+        <div style="background: rgba(0,0,0,1);">
+            <div class="navbar-top flex justify-between items-center p-3 text-xl font-bold text-white uppercase tracking-wider secondary-btn shadow-xl">
+                <h1>
+                    <?php echo $_SESSION['displayName'] ?>
+                </h1>
+                <button class="text-lg rounded-md px-3 py-1 bg-red-500 hover:bg-red-600 shadow-md" style="border: 1px solid rgba(255,255,255, 0.3);">Logout</button>
+            </div>
+        </div>
+        <div class="mt-6 flex flex-col items-center">
+            <div class="p-6 border shadow-md rounded-md bg-white w-36 h-36 text-center">
+                <i class="ml-2 far fa-address-book text-7xl primary"></i>
+                <h2 class="p-2 uppercase text-xl text-gray-800 font-bold tracking-wider">Ordini</h2>
+            </div>
+        </div>
+    </div>
+    <?php include 'common/navbar.php' ?>
 </body>
 
 </html>
